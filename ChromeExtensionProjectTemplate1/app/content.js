ea187c3299f641bfbfd277e91e21a20e1a6c5c44
Cheng Huang <daily2432121@gmail.com>
2014-10-27 02:19:53 -0400

    Logic update use simplemodal as popup js lib handle DOM level hyperlink generation for several cases

--- ChromeExtensionProjectTemplate1/app/content.js
+++ ChromeExtensionProjectTemplate1/app/content.js
@@ -5,8 +5,12 @@ chrome.runtime.onMessage.addListener(
     });
 
 $("#header").ready(function () {
-    $("#btnBeat").remove();
-    addButton();
+    //$("#btnBeat").remove();
+    //addButton();
+    var timer = $.timer(function () {
+        timerTask();
+    });
+    timer.set({ time: 10000, autostart: true });
 });
 
 
@@ -20,16 +24,28 @@ function timerTask() {
         $(".message_content").each(function (index) {
             var h = $(this).html();
             if (h.indexOf('class="rallyItem') >= 0) return true;
-            var replaced = hyperlinkify(h);
-            if (replaced != null) {
-                $(this).html(replaced);
-            }
-
+            hyperlinkifyInnerMost($(this)[0]);
         });
 
         $(".rallyItem").ready(function () {
             $(".rallyItem").click(function () {
-                $(this).colorbox({ iframe: true, width: "80%", height: "80%", opacity: 0, closeButton: true });
+                //$(this).colorbox({ iframe: true, width: "80%", height: "80%",opacity:0,closeButton:true });
+                var h = $(window).height() * 0.8;
+                var w = $(window).width() * 0.8;
+                $.modal('<iframe id="rallyItemIframe" src="' + $(this).attr("rallyItem") + '" scrolling="no" style="overflow:hidden; border:0; position:relative; height:' + h + 'px; width:' + w + 'px;">', {
+                    closeHTML: "",
+                    containerCss: {
+                        backgroundColor: "#fff",
+                        borderColor: "#fff",
+                        height: h,
+                        padding: 0,
+                        width: w,
+                    },
+                    overlayClose: true,
+                    escClose: true
+                });
+                $("#simplemodal-data").css("height", $("#rallyItemIframe").height());
+                $("#simplemodal-data").css("overflow", "hidden");
             });
         });
 
@@ -57,16 +73,28 @@ function addButton() {
             $(".message_content").each(function (index) {
                 var h = $(this).html();
                 if (h.indexOf('class="rallyItem') >= 0) return true;
-                var replaced = hyperlinkify(h);
-                if (replaced != null) {
-                    $(this).html(replaced);
-                }
-                
+                hyperlinkifyInnerMost($(this)[0]);
             });
 
             $(".rallyItem").ready(function () {
                 $(".rallyItem").click(function () {
-                    $(this).colorbox({ iframe: true, width: "80%", height: "80%",opacity:0,closeButton:true });
+                    //$(this).colorbox({ iframe: true, width: "80%", height: "80%",opacity:0,closeButton:true });
+                    var h = $(window).height() * 0.8;
+                    var w = $(window).width() * 0.8;
+                    $.modal('<iframe id="rallyItemIframe" src="' + $(this).attr("rallyItem") + '" scrolling="no" style="overflow:hidden; border:0; position:relative; height:' + h + 'px; width:' + w + 'px;">', {
+                        closeHTML: "",
+                        containerCss: {
+                            backgroundColor: "#fff",
+                            borderColor: "#fff",
+                            height: h,
+                            padding: 0,
+                            width: w,
+                        },
+                        overlayClose: true,
+                        escClose:true
+                    });
+                    $("#simplemodal-data").css("height", $("#rallyItemIframe").height());
+                    $("#simplemodal-data").css("overflow", "hidden");
                 });
             });
         });
@@ -76,25 +104,65 @@ function addButton() {
 }
 
 
-
 function hyperlinkify(text) {
     var userstoryPrefex = "https://rally1.rallydev.com/#/13073500360d/detail/userstory/";
     var defectPrefex = "https://rally1.rallydev.com/#/13073500360d/detail/defect/";
     var regex = /\b(us\d{3,10}|de\d{3,10})\b/ig;
+
     //alert(text);
     var result = text;
     var item;
     while ((item = regex.exec(text)) != null) {
-        console.log(item[0]);
         if (/^de/i.test(item[0])) {
-            result = result.replace(item[0], '<a class="rallyItem" href="' + defectPrefex + item[0] + '"  rel="noreferrer" target="_blank">' + item[0] + '</a>');
+            result = result.replace(item[0], '<a class="rallyItem" style="color:#00f;font-weight:bold;" href="#" rallyItem="' + defectPrefex + item[0] + '"  rel="noreferrer" target="_self">' + item[0] + '</a>');
         }
         else {
-            result = result.replace(item[0], '<a class="rallyItem" href="' + userstoryPrefex + item[0] + '" rel="noreferrer" target="_blank">' + item[0] + '</a>');
+            result = result.replace(item[0], '<a class="rallyItem" style="color:#00f;font-weight:bold;"href="#" rallyItem="' + userstoryPrefex + item[0] + '" rel="noreferrer" target="_self">' + item[0] + '</a>');
             //result = result.replace(item[0], '<a href="#" class="rallyItem" rel="noreferrer" target="_blank">' + item[0] + '</a>');
         }
 
 
     }
     return result;
+}
+
+
+function hyperlinkifyInnerMost(node) {
+    if (node.nodeType == 3) {
+        // Filter out text nodes that contain only whitespace
+        if (!/^\s*$/.test(node.data)) {
+            var replace = hyperlinkify(node.data);
+            wrapMatchesInNode(node, replace);
+            return;
+        }
+    } else if (node.hasChildNodes()) {
+        for (var i = 0, len = node.childNodes.length; i < len; ++i) {
+            hyperlinkifyInnerMost(node.childNodes[i]);
+        }
+    }
+}
+
+function wrapMatchesInNode(textNode, replacementText) {
+
+    var temp = document.createElement('div');
+
+    temp.innerHTML = replacementText;
+
+    // temp.innerHTML is now:
+    // "\n    This order's reference number is <a href="/order/RF83297">RF83297</a>.\n"
+    // |_______________________________________|__________________________________|___|
+    //                     |                                      |                 |
+    //                 TEXT NODE                             ELEMENT NODE       TEXT NODE
+
+    // Extract produced nodes and insert them
+    // before original textNode:
+    while (temp.firstChild) {
+        //console.log(temp.firstChild.nodeType);
+        textNode.parentNode.insertBefore(temp.firstChild, textNode);
+    }
+    // Logged: 3,1,3
+
+    // Remove original text-node:
+    textNode.parentNode.removeChild(textNode);
+
 }
--- ChromeExtensionProjectTemplate1/app/popup.js
+++ ChromeExtensionProjectTemplate1/app/popup.js
@@ -13,8 +13,7 @@ function initialize(tabId) {
     if (!isMyScriptLoaded())
     {
         chrome.tabs.executeScript(tabId, { file: "jquery-1.11.1.min.js", allFrames: true });
-        chrome.tabs.insertCSS(tabId, { file: "colorbox.css", allFrames: true });
-        chrome.tabs.executeScript(tabId, { file: "jquery.colorbox.js", allFrames: true });
+        chrome.tabs.executeScript(tabId, { file: "jquery.simplemodal.js", allFrames: true });
         chrome.tabs.executeScript(tabId, { file: "jquery.timer.js", allFrames: true });
         chrome.tabs.executeScript(tabId, { file: "content.js", allFrames: true });
     }
